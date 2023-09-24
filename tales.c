e8d0cbd654d3499c5232330795115853fb7d2c21
Talescruzs <tcs2017@hotmail.com>
2023-09-24 17:19:55 -0300

    acho que foi

--- tales.c
+++ tales.c
@@ -10,12 +10,16 @@ int pegaDigito(int numero, int posicao){
 
     return numero%10;
 }
+float mediaSimples(int total, int qtd){
+    return (float)total/qtd;
+}
 
 int main(){
     int matricula, soma=0, i, DV, op=0;
 
-    printf("Matricula: ");
+    printf("Insira a matrícula: ");
     scanf("%d", &matricula);
+    matricula=matricula%100000;
     for(i=5; i>=1; i--){
         soma+=pegaDigito(matricula, i)*(4+i);
     }
@@ -24,59 +28,113 @@ int main(){
     if(DV>9){
         DV=pegaDigito(DV, 1);
     }
-    int  cont=0, contPassou, contRodou, contAproveitou;
-    float nota=0, cargoHoraria=0, totalNota=0, totalHora=0, notaPassou=0, notaRodou=0, notaAproveitou=0;
 
-    printf(" Insira os valores:\n");
-    while(nota>=0&&cargoHoraria>=0){
-        printf("Nota: ");
+    int contPassou=0, contRodou=0, contAproveitou=0, contRodouSemNota=0, ap;
+    float nota=0, hora=0, notaMinima;
+    float notaPassou=0, notaRodou=0, notaAproveitou=0;
+    float horaPassou=0, horaRodou=0, horaAproveitou=0, presenca;
+    float horaRodouSemNota=0;
+    float MGA=0, MTD=0, IDA=0, PAA=0;
+    float MDA=0, MDR;
+
+    printf("Insira os valores solicitados:\n");
+    printf(" Qual a nota minima para a aprovação nas disciplinas? ");
+    scanf("%f", &notaMinima);
+    while(nota>=0&&hora>=0){
+        printf(" Nota tirada na disciplina: ");
         scanf("%f", &nota);
-        printf("Carga horária: ");
-        scanf("%f", &cargoHoraria);
-        if(nota>=7&&nota<=10){
-            printf("Passou\n");
-            cont++;
-            contPassou++;
-            totalNota+=nota;
-            notaPassou+=nota;
-            totalHora+=cargoHoraria;
+        if(nota>=0&&nota<=10){
+            printf(" Carga horária da disciplina: ");
+            scanf("%f", &hora);
+            if(hora>0){
+                printf(" Quantidade de horas presente na disciplina: ");
+                scanf("%f", &presenca);
+                if(presenca>=hora*0.75&&nota>=notaMinima){
+                    printf(" Esta cadeira foi aproveitada ou dispensada? (1 para sim) ");
+                    scanf("%d", &ap);
+                }
+            }
         }
-        else if(nota>10){
-            printf("Valor inválido inserido\n");
-        }
-        else if(nota>=0&&nota<7){
-            cont++;
-            contRodou++;
-            totalNota+=nota;
-            notaRodou+=nota;
-            totalHora+=cargoHoraria;
+        if(nota>=0){
+            if(nota>10||hora<=0){
+                printf("  Valor inválido inserido\n");
+            }
+            else if(presenca>=hora*0.75){
+                if(nota>=notaMinima){
+                    if(ap!=1){
+                        contPassou++;
+                        notaPassou+=nota;
+                        horaPassou+=hora;
+                    }
+                    else{
+                        contAproveitou++;
+                        notaAproveitou+=nota;
+                        horaAproveitou+=hora;
+                    }
+                    MDA+=nota*hora;
+                }
+                
+                else{
+                    contRodou++;
+                    notaRodou+=nota;
+                    horaRodou+=hora;
+                    MDR+=nota*hora;
+                }
+            }
+            else{
+                contRodouSemNota++;
+                horaRodouSemNota+=hora;
+                MDR+=nota*hora;
+            }
         }
     } 
 
+    MTD=MDA-MDR;
+    MGA=mediaSimples(notaPassou, contPassou);
+    IDA+=mediaSimples(notaPassou, contPassou)*(horaPassou); // Média final das disciplinas aprovadas com nota * carga horária das disciplinas aprovadas com nota
+    IDA+=mediaSimples(notaAproveitou, contAproveitou) * horaAproveitou; //  Média das médias das disciplinas aprovadas sem nota * carga horária das disciplinas aprovadas sem nota
+    IDA-=(notaMinima-mediaSimples(notaAproveitou+notaPassou+notaRodou, contRodou+contPassou+contAproveitou+contRodouSemNota))*horaRodou; // difetenca da nota minima para a media final (todas as disciplinas)*carga horaria das disciplinas que rodou com nota
+    IDA-=notaMinima*horaRodouSemNota;
+    PAA=((contPassou+contAproveitou)*100)/(contRodou+contRodouSemNota+contPassou+contAproveitou);
+    printf("%d\n", contPassou);
+    printf("%d\n", contAproveitou);
+    printf("%d\n", contRodou);
+    printf("%d\n", contRodouSemNota);
+
+
     while(op!=5){
-        printf("Opções de calculo do aluno %d-%d:\n", matricula, DV);
-        printf("1- Média Geral Acumulada (MGA)\n");
-        printf("2- Índice de desempenho Acadêmico (IDA)\n");
-        printf("3- Média Total de Disciplinas (MTD)\n");
-        printf("4- Percentual de Aproveitamento do Aluno (PAA)\n");
-        printf("5- Sair\n");
+        printf("Opções de cálculo do aluno %d-%d:\n", matricula, DV);
+        printf(" 1- Média Geral Acumulada (MGA)\n");
+        printf(" 2- Índice de desempenho Acadêmico (IDA)\n");
+        printf(" 3- Média Total de Disciplinas (MTD)\n");
+        printf(" 4- Percentual de Aproveitamento do Aluno (PAA)\n");
+        printf(" 5- Sair\n");
+        printf(" Selecione a opção desejada para calcular: ");
         scanf("%d", &op);
         switch(op){
             case 1:
-                printf(" Média Geral Acumulada (MGA):\n");
-                printf("MGA = %f\n", notaPassou/contPassou);
+                printf("  Média Geral Acumulada (MGA):\n");
+                printf("   MGA = %f\n", MGA);
                 break;
             case 2:
-                printf("1- Média Geral Acumulada (MGA)\n");
+                printf("  Índice de desempenho Acadêmico (IDA)\n");
+                printf("   IDA = %f\n", IDA);
                 break;
             case 3:
-                printf("1- Média Geral Acumulada (MGA)\n");
+                printf("  Média Total de Disciplinas (MTD)\n");
+                printf("   MTD = %f\n", MTD);
                 break;
             case 4:
-                printf("1- Média Geral Acumulada (MGA)\n");
+                printf("  Percentual de Aproveitamento do Aluno (PAA)\n");
+                printf("   PAA = %f\n", PAA);
+                if(PAA==100){
+                    printf("   Parabéns ao aluno, passou em todas as disciplinas :)");
+                }
+                break;
+            case 5:
                 break;
             default:
-                printf("Opção inválida");
+                printf("  Opção inválida");
         }
 
     }
