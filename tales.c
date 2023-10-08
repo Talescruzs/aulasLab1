b9e341f99574e68791de85ec8573917affe46bbd
Talescruzs <tcs2017@hotmail.com>
2023-10-08 13:53:31 -0300

    completando trabalho

--- tales.c
+++ tales.c
@@ -116,36 +116,44 @@ int main(){
         printf(" %f |", v4[i]);
     }
 
-    printf("\n");
-    for(i=0; i<t*2; i++){
-        printf(" %f |", somado[i]);
-    }
-
     float v5[t], v6[t];
     flag1=0, flag2=0;
-    for(i=0, j=t*2-1; i<t*2; i++, j--){
-        if(j%2==0){
+    for(i=0, j=t-1, k=t-1; i<t*2; i++){
+        if(i%2==0){
             if(flag1==0){
-                v5[i]=v4[j];
+                v5[j]=v4[i]-somado[i];
                 flag1=1;
+                j--;
             }
             else{
-                v3[i]=v2[(j*2)+1];
+                v6[k]=v4[i]-somado[i];
                 flag1=0;
+                k--;
             }
         }
         else{
             if(flag2==0){
-                v3[i]=v2[j*2];
+                v6[k]=v4[i]-somado[i];
                 flag2=1;
+                k--;
             }
             else{
-                v3[i]=v1[(j*2)+1];
+                v5[j]=v4[i]-somado[i];
                 flag2=0;
-                j++;
+                j--;
             }
 
         }
+    }
+    printf("\n");
+    printf("<v5: |");
+    for(i=0; i<t; i++){
+        printf(" %f |", v5[i]);
+    }
+    printf("\n");
+    printf("<v6: |");
+    for(i=0; i<t; i++){
+        printf(" %f |", v6[i]);
     }
 
     return 0;
