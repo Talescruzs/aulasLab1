8385d6044c69b49e58fbfee062ee29c6e8fc7b4a
Talescruzs <tcs2017@hotmail.com>
2023-10-21 18:10:06 -0300

    add variedade de pecas e disposicao inicial

--- trabalhoFinal/main.c
+++ trabalhoFinal/main.c
@@ -3,7 +3,27 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/keyboard.h>
 
+#include "peca.h"
+
 int main (){
+  int qtdPecas = 24;
+  struct Peca pecas[qtdPecas];
+  int i, j, pos=0;
+
+  for(int i = 0; i<6; i++){
+    for(int j = 0; j<2; j++){
+      struct Peca teste = {140+(i*76), 150+(j*85), 0};
+      pecas[pos] = teste;
+      pos++;
+    }
+  }
+  for(int i = 0; i<6; i++){
+    for(int j = 4; j<6; j++){
+      struct Peca teste = {140+(i*76), 150+(j*85), 1};
+      pecas[pos] = teste;
+      pos++;
+    }
+  }
 
   al_init();
   al_init_font_addon();
@@ -18,7 +38,11 @@ int main (){
   ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
 
   ALLEGRO_BITMAP * bg = al_load_bitmap("./img/fundo.png");
-  ALLEGRO_BITMAP * peca = al_load_bitmap("./img/peca1.png");
+
+  ALLEGRO_BITMAP * peca1 = al_load_bitmap("./img/peca1.png");
+  ALLEGRO_BITMAP * peca2 = al_load_bitmap("./img/peca2.png");
+  ALLEGRO_BITMAP * seleciona = al_load_bitmap("./img/seleciona.png");
+  ALLEGRO_BITMAP * opcao = al_load_bitmap("./img/opcao.png");
 
   ALLEGRO_EVENT_QUEUE * event_queue = al_create_event_queue();
   al_register_event_source(event_queue, al_get_display_event_source(display));
@@ -34,16 +58,22 @@ int main (){
     }
 
     al_draw_bitmap(bg, 0, 0, 0);
-    for(int i = 0; i<6; i++){
-      for(int j = 0; j<6; j++){
-        al_draw_bitmap(peca, 140+(i*76), 150+(j*85), 0);
+    for(int i=0; i<qtdPecas; i++){
+      if(pecas[i].time==1){
+        al_draw_bitmap(peca1, pecas[i].posX, pecas[i].posY, 0);
+      }
+      else{
+        al_draw_bitmap(peca2, pecas[i].posX, pecas[i].posY, 0);
       }
     }
     al_flip_display();
   }
 
   al_destroy_bitmap(bg);
-  al_destroy_bitmap(peca);
+  al_destroy_bitmap(peca1);
+  al_destroy_bitmap(peca2);
+  al_destroy_bitmap(seleciona);
+  al_destroy_bitmap(opcao);
   al_destroy_font(font);
   al_destroy_display(display);
   al_destroy_event_queue(event_queue);
---
+++ trabalhoFinal/peca.h
@@ -0,0 +1,9 @@
+#ifndef __PERSONAGEM_H_
+#define __PERSONAGEM_H_
+
+struct Peca{
+    int posX, posY;
+    int time;
+};
+
+#endif
