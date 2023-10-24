86a1c35e80b961e54b64495f9bf47d0db8cd2d90
Talescruzs <tcs2017@hotmail.com>
2023-10-24 18:50:52 -0300

    arrumando movimento das pecas do jogador

--- trabalhoFinal/peca.c
+++ trabalhoFinal/peca.c
@@ -38,15 +38,20 @@ void movePeca(int posx, int posy, struct Posicao posicoes[6][6], int*rodada){
         for(int j=0; j<6; j++){
             if(posicoes[i][j].opcao==1){
                 if((posx>=posicoes[i][j].posX && posx<=posicoes[i][j].posX+60)&&(posy>=posicoes[i][j].posY && posy<=posicoes[i][j].posY+60)){
-                    posicoes[i][j].estado=1;
+                    posicoes[i][j].estado=pecaMov.estado;
                     posicoes[i][j].opcao=0;
                     posicoes[i][j].selecionada=0;
-                    *rodada++;
+                    rodada++;
                     flag=1;
                 }
             }
+        }
+    }
+    for(int i=0; i<6; i++){
+        for(int j=0; j<6; j++){
             if(posicoes[i][j].posX==pecaMov.posX&&posicoes[i][j].posY==pecaMov.posY && flag==1){
                 posicoes[i][j].estado=0;
+                posicoes[i][j].opcao=0;
                 posicoes[i][j].selecionada=0;
                 flag=0;
             }
